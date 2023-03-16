15e3d033b82ebf24ed4e63a89c0c2b908f844a86
salma Elmaghawry <112850475+salma-elmaghawry@users.noreply.github.com>
2023-03-16 23:07:31 +0200

    Add files via upload

---
+++ kindness/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	int i;
+	for ( i = 0; i < size(s); i++)
+	{
+		cout << size(s);
+		return 0;
+	}
+}
